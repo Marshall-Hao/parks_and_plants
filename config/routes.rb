5872a896ab0b544675fd043d0e119189405a4d4b
marshall chen <marshallchan666@outlook.com>
2021-07-14 17:18:19 +0800

    done but with a little bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: "gardens#index"
-  resources :gardens
+  resources :gardens do
+    resources :plants, only: [:create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :plants, only: :destroy
 end
