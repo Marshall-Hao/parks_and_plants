e63c9c3f96a848736677070954c13e1e576af049
marshall chen <marshallchan666@outlook.com>
2021-07-14 15:12:53 +0800

    frontend set-up done

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,17 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
+#   # db/seeds.rb
+Garden.destroy_all if Rails.env.development?
+
+Garden.create!(
+  name: "My Little Garden",
+  banner_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_1.jpg"
+)
+
+Garden.create!(
+  name: "My Other Garden",
+  banner_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_2.jpg"
+)
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
